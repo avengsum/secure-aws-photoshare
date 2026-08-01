b969a65e6293f895a4a4f94b3ab1b6fc1c75af85
avengsum <sy710481@gmail.com>
2026-08-02 04:18:33 +0530

    ok fixed some bug

--- terraform/github_policy.tf
+++ terraform/github_policy.tf
@@ -29,6 +29,24 @@ data "aws_iam_policy_document" "github_permissions" {
       "arn:aws:ssm:*:*:document/*"
     ]
   }
+
+  statement {
+
+    sid    = "ECRPush"
+    effect = "Allow"
+
+    actions = [
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:CompleteLayerUpload",
+      "ecr:InitiateLayerUpload",
+      "ecr:PutImage",
+      "ecr:UploadLayerPart"
+    ]
+
+    resources = [
+      aws_ecr_repository.app.arn
+    ]
+  }
 }
 
 resource "aws_iam_policy" "github_actions" {
@@ -41,7 +59,7 @@ resource "aws_iam_policy" "github_actions" {
 
 resource "aws_iam_role_policy_attachment" "github_actions" {
 
-  role       = aws_iam_role.github_actions.name
+  role = aws_iam_role.github_actions.name
 
   policy_arn = aws_iam_policy.github_actions.arn
 
