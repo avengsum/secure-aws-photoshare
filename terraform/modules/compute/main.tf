0b055e83754b4275bcaade70de0be3aac28a6275
avengsum <sy710481@gmail.com>
2026-08-03 01:14:32 +0530

    Require IMDSv2 for EC2 instances

--- terraform/modules/compute/main.tf
+++ terraform/modules/compute/main.tf
@@ -54,6 +54,13 @@ resource "aws_launch_template" "app" {
     enabled = true
   }
 
+  metadata_options {
+    http_endpoint               = "enabled"
+    http_tokens                 = "required"
+    http_put_response_hop_limit = 2
+    instance_metadata_tags      = "disabled"
+  }
+
   tag_specifications {
     resource_type = "instance"
     tags = {
