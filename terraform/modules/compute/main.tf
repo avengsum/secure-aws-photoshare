c8bd17a0c6e3719262ba076e2b7239b17d27d67f
avengsum <sy710481@gmail.com>
2026-08-03 01:28:41 +0530

    fixed cd error

--- terraform/modules/compute/main.tf
+++ terraform/modules/compute/main.tf
@@ -57,7 +57,7 @@ resource "aws_launch_template" "app" {
   metadata_options {
     http_endpoint               = "enabled"
     http_tokens                 = "required"
-    http_put_response_hop_limit = 2
+    http_put_response_hop_limit = 1
     instance_metadata_tags      = "disabled"
   }
 
